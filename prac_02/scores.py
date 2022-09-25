f285b7693b34b6b1c90b187cb737ffee1f5f4160
Anthony Maxwell <Anthony.Maxwell@my.jcu.edu.au>
2022-09-25 20:43:06 +1000

    Added functions to the program

--- prac_02/scores.py
+++ prac_02/scores.py
@@ -1,12 +1,28 @@
 """CP1404 Practical - Scores.py"""
 
-score = float(input("Enter score: "))
-if score >= 0 and score <= 100:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
+from random import randint
+
+
+def main():
+    user_score = float(input("Enter your score: "))
+    user_result = calculate_results(user_score)
+    print("Your score is: ", user_result)
+    random_score = randint(0, 100)
+    random_result = calculate_results(random_score)
+    print(f"Random score is: {random_score} it is: {random_result}")
+
+
+def calculate_results(score):
+    if score >= 0 and score <= 100:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        else:
+            return "Bad"
     else:
-        print("Bad")
-else:
-    print("invalid score")
+        return "Invalid Score"
+    pass
+
+
+main()
