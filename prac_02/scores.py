e3efd2b95edff891d672ec8d455b4142217d0740
Anthony Maxwell <Anthony.Maxwell@my.jcu.edu.au>
2022-09-25 20:30:06 +1000

    Initial commit before adding functions

---
+++ prac_02/scores.py
@@ -0,0 +1,12 @@
+"""CP1404 Practical - Scores.py"""
+
+score = float(input("Enter score: "))
+if score >= 0 and score <= 100:
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+else:
+    print("invalid score")
