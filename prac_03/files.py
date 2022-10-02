46eea1cdd63e6e1034c9676c47a047d059bb812b
Anthony Maxwell <Anthony.Maxwell@my.jcu.edu.au>
2022-10-02 18:28:47 +1000

    Wrote program 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -10,3 +10,20 @@ in_file = open("name.txt", 'r')
 name = in_file.read()
 print(f"Your name is {name}")
 in_file.close()
+
+# Program 3:
+in_file = open("numbers.txt", 'r')
+first_number = int(in_file.readline())
+second_number = int(in_file.readline())
+print(first_number + second_number)
+in_file.close()
+
+in_file = open("numbers.txt", 'r')
+total_number = 0
+for line in in_file:
+    number = int(line)
+    total_number += number
+print(total_number)
+in_file.close()
+
+
