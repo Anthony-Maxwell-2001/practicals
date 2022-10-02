26211dc0d49075d1e6c5fea2ab5718dd8c6eef0b
Anthony Maxwell <Anthony.Maxwell@my.jcu.edu.au>
2022-10-02 18:20:46 +1000

    Wrote second program for reading the file and printing name

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,3 +4,9 @@ name = input("Name: ")
 out_file = open("name.txt", 'w')
 print(name, file=out_file)
 out_file.close()
+
+# Program 2:
+in_file = open("name.txt", 'r')
+name = in_file.read()
+print(f"Your name is {name}")
+in_file.close()
