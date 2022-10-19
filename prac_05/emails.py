1f8da1d88ad0ee9c5326a5b68c99e82bbe7568d9
Anthony Maxwell <Anthony.Maxwell@my.jcu.edu.au>
2022-10-19 22:15:58 +1000

    Finished main function and wrote a function for displaying emails & names

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,10 +6,16 @@ Actual:
 
 
 def main():
+    email_to_name = {}
     email = input("Email: ")
     while email != "":
         name = extract_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n): ").upper()
+        if confirmation != 'Y' and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
         email = input("Email: ")
+    display_names_and_emails(email_to_name)
 
 
 def extract_name_from_email(email):
@@ -17,6 +23,11 @@ def extract_name_from_email(email):
     names = first_half.split('.')
     name = " ".join(names).title()
     return name
+
+
+def display_names_and_emails(email_to_name):
+    for name, email in email_to_name.items():
+        print(f"{name} ({email})")
 
 
 main()
