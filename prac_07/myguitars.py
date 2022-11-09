ff5615b5b90d9bc6db0b59f190982869abcd81c2
Anthony Maxwell <Anthony.Maxwell@my.jcu.edu.au>
2022-11-10 08:55:58 +1000

    Complete first part of More Guitars exercise

---
+++ prac_07/myguitars.py
@@ -0,0 +1,27 @@
+"""
+CP1404 myguitars.py
+
+Anthony Maxwell
+"""
+import csv
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
+
+    in_file = open("guitars.csv", 'r')
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        guitars.append(guitar)
+    in_file.close()
+
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
+
+
+main()
+
+
