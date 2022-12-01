6605caeef367639ee1a8b54edd6879fd70f7d880
Anthony Maxwell <anthony.maxwell@my.jcu.edu.au>
2022-12-01 21:37:34 +1000

    Completed wiki.py

---
+++ prac_10/wiki.py
@@ -0,0 +1,11 @@
+import wikipedia
+
+user_summary = input("Enter a page to get a summary for: ")
+while user_summary != '':
+    try:
+        page = wikipedia.page(user_summary, auto_suggest=False)
+        print(f"{page.title} \n {page.url}")
+        print(wikipedia.summary(user_summary, auto_suggest=False))
+    except wikipedia.exceptions.DisambiguationError as error:
+        print(error.options)
+    user_summary = input("Enter a page to get a summary for: ")
