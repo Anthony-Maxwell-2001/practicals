0592e5fdc78e24a1876e69ed254514c0f941a867
Anthony Maxwell <anthony.maxwell@my.jcu.edu.au>
2022-12-01 21:38:44 +1000

    Fix formatting

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,7 +4,7 @@ user_summary = input("Enter a page to get a summary for: ")
 while user_summary != '':
     try:
         page = wikipedia.page(user_summary, auto_suggest=False)
-        print(f"{page.title} \n {page.url}")
+        print(f"{page.title}\n{page.url}")
         print(wikipedia.summary(user_summary, auto_suggest=False))
     except wikipedia.exceptions.DisambiguationError as error:
         print(error.options)
